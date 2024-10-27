d23b88f224e17b481ce6916f6dfd059b066512d1
poojamanoj01 <poojamanoj3002@gmail.com>
2024-10-27 11:59:30 +0530

    modified login nd dashbord

--- login/main.js
+++ login/main.js
@@ -1,15 +1,28 @@
-const login = ()=>{
-    const username = usernameInput.value
-    const password = pswdInput.value
+// const login = ()=>{
+//     const username = usernameInput.value
+//     const password = pswdInput.value
 
-    if(username && password){
-        // store username & password permananetly
-        localStorage.setItem("username",username)
-        localStorage.setItem("password",password)
-        // navigate to dashboard
-        window.location = "dashbord.html"
+//     if(username && password){
+//         // store username & password permananetly
+//         localStorage.setItem("username",username)
+//         localStorage.setItem("password",password)
+//         // navigate to dashboard
+//         window.location = "dashbord.html"
 
-    }else{
-        alert("please fill the form completely!!!!")
+//     }else{
+//         alert("please fill the form completely!!!!")
+//     }
+// }
+
+
+const login = () => {
+    const username = document.getElementById("usernameInput").value;
+    const password = document.getElementById("pswdInput").value;
+
+    if (username && password) {
+        localStorage.setItem("username", username);
+        window.location = "dashbord.html"; // make sure the filename matches exactly
+    } else {
+        alert("Please fill out the form completely!");
     }
-}
+};
